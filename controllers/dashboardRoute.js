618def0b18b8ec4adb77d14270f7a6239785ee97
alisonarmstrong <alarmstrong15@gmail.com>
2021-06-16 11:08:37 -0600

    updated controllers

---
+++ controllers/dashboardRoute.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async, (req,res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }],
+        });
+        const user = userData.get({ plain: true });
+        res.render('dashboard', {
+            ...user,
+            logged_in: req.session.logged_in,
+            logged_name: req.session.logged_name,
+        });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.get('/newpost', withAith, (req,res) => {
+    try {
+        if(req.session.logged_in) {
+            res.render('new-post',{
+                logged_in: req.session.logged_in
+            });
+            return;
+        }
+        res.redirect('/dashboard');
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.get('/edit/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [{ model: User }],
+        });
+        const post = postData.get({ plain: true });
+        res.render('edit', {
+            post,
+            logged_in: req.session.logged_in
+        });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+module.exports = router;
